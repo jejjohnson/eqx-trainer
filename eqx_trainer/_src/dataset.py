d73b368b5d4dfe93557f966aae777b2bf4e5e22b
Juan Emmanuel Johnson <jemanjohnson34@gmail.com>
2023-06-14 22:30:26 +0200

    Added NumpyLoader.

---
+++ eqx_trainer/_src/dataset.py
@@ -0,0 +1,33 @@
+from torch.utils.data import DataLoader
+from eqx_trainer._src.data import numpy_collate
+
+
+class NumpyLoader(DataLoader):
+    def __init__(
+        self,
+        dataset,
+        batch_size=1,
+        shuffle=False,
+        sampler=None,
+        batch_sampler=None,
+        num_workers=0,
+        pin_memory=False,
+        drop_last=False,
+        timeout=0,
+        worker_init_fn=None,
+        persistent_workers=False,
+    ):
+        super(self.__class__, self).__init__(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            sampler=sampler,
+            batch_sampler=batch_sampler,
+            num_workers=num_workers,
+            collate_fn=numpy_collate,
+            pin_memory=pin_memory,
+            drop_last=drop_last,
+            timeout=timeout,
+            worker_init_fn=worker_init_fn,
+            persistent_workers=persistent_workers,
+        )
